447851c4bc17d7111c01ca939b35d50b34c242d1
droser <droser@cs.washington.edu>
2025-03-22 18:23:14 -0700

    haha moved

---
+++ Luminarily/LuminarilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminarilyApp.swift
+//  Luminarily
+//
+//  Created by Dominic Roser on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct LuminarilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LuminarilyTests/LuminarilyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LuminarilyTests.swift
+//  LuminarilyTests
+//
+//  Created by Dominic Roser on 3/22/25.
+//
+
+import Testing
+@testable import Luminarily
+
+struct LuminarilyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
